00b490aa8a3c2b37c0b6dc2797583c96e9fca7ed
Ryan Leavitt <hopelessgains@gmail.com>
2021-03-10 21:45:44 -0700

    added step 7 that i spaced

--- server/index.js
+++ server/index.js
@@ -5,9 +5,10 @@ const mu = require('./controllers/messages_controller')
 const app = express()
 
 app.use(express.json())
+app.use(express.static(__dirname + '/../public/build'));
 
 const port = 3001
-app.listen(port,() => console.log('toss a coin to your local witcher ' + port))
+app.listen(port, () => console.log('toss a coin to your local witcher ' + port))
 
 app.post('/api/messages', mu.create)
 app.get('/api/messages', mu.read)
